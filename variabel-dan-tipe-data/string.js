5ff4d7577d744b4cafb317fbf3b055405268edbf
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-11 19:35:10 +0700

    Variabel dan Tipe data

---
+++ variabel-dan-tipe-data/string.js
@@ -0,0 +1,10 @@
+// string adalah tipe data yang digunakan untuk merepresentasikan teks
+
+// const digunakan untuk mendeklarasikan variabel yang nilainya tidak dapat diubah
+const headText = "Hello, "
+const tailText = "how are you?"
+const fullText = headText + tailText // menggabungkan dua string menjadi satu string
+let message = fullText
+
+// untuk menampilkan teks menggunakan console.log
+console.log(fullText); // console.log akan memanggil string yang sudah digabungkan
