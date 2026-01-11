980f6c958868d3afb2f8ebba0ef29481abf4e68d
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-11 19:38:06 +0700

    Variabel dan Tipe data

--- variabel-dan-tipe-data/string.js
+++ variabel-dan-tipe-data/string.js
@@ -4,7 +4,6 @@
 const headText = "Hello, "
 const tailText = "how are you?"
 const fullText = headText + tailText // menggabungkan dua string menjadi satu string
-let message = fullText
 
 // untuk menampilkan teks menggunakan console.log
 console.log(fullText); // console.log akan memanggil string yang sudah digabungkan
