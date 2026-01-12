b8ab90ef612d664e1181424aa49c7f68804a729a
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-12 17:22:56 +0700

    Null and Undefined

---
+++ 1.variabel-dan-tipe-data/null_undefined.js
@@ -0,0 +1,14 @@
+// null adalah sebuah tipe data  yang merepresentasikan tidak ada nilai atau nilai kosong  
+const dataKosong = null;
+console.log("nilai dari dataKosong: " + dataKosong);
+// berarti nilai dari dataKosong adalah null atau kosong
+
+// berbeda dari null, undefined adalah sebuah tipe data yang merepresentasikan variabel yang belum memiliki nilai yang ditetapkan
+
+let dataTidakDitetapkan;
+console.log("nilai dari dataTidakDitetapkan: " + dataTidakDitetapkan);
+// berarti nilai dari dataTidakDitetapkan adalah undefined karena belum ditetapkan nilainya yang spesifik
+
+// null dapat digunakan secara eksplisit untuk menunjukkan bahwa sebuah variabel tidak memiliki nilai dan akan diisi dengan nilai tertentu, sedangkan undefined biasanya terjadi secara otomatis ketika sebuah variabel dideklarasikan tetapi belum diinisialisasi dengan nilai apapun.
+// undefined sering kali menunjukkan bahwa ada sesuatu yang belum diatur atau belum diinisialisasi dalam kode, sedangkan null lebih sering digunakan untuk menunjukkan secara eksplisit bahwa tidak ada nilai yang diinginkan atau relevan.
+
