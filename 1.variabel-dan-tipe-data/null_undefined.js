58be66dc0927f5de3ea2091d00eee783ed9ce8b1
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-12 17:23:43 +0700

    Null and Undefined

--- 1.variabel-dan-tipe-data/null_undefined.js
+++ 1.variabel-dan-tipe-data/null_undefined.js
@@ -11,4 +11,5 @@ console.log("nilai dari dataTidakDitetapkan: " + dataTidakDitetapkan);
 
 // null dapat digunakan secara eksplisit untuk menunjukkan bahwa sebuah variabel tidak memiliki nilai dan akan diisi dengan nilai tertentu, sedangkan undefined biasanya terjadi secara otomatis ketika sebuah variabel dideklarasikan tetapi belum diinisialisasi dengan nilai apapun.
 // undefined sering kali menunjukkan bahwa ada sesuatu yang belum diatur atau belum diinisialisasi dalam kode, sedangkan null lebih sering digunakan untuk menunjukkan secara eksplisit bahwa tidak ada nilai yang diinginkan atau relevan.
-
+// kesimpulannya, null dan undefined keduanya merepresentasikan ketiadaan nilai, tetapi mereka digunakan dalam konteks yang berbeda dan memiliki arti yang berbeda dalam pemrograman.
+// null adalah penetapan nilai kosong secara eksplisit, sedangkan undefined menunjukkan bahwa nilai belum ditetapkan.
