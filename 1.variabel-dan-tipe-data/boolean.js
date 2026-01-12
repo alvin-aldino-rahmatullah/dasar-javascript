84f8fa290039f7a51a41df3468e61d308f2dcb49
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-12 17:03:32 +0700

    New Name

---
+++ 1.variabel-dan-tipe-data/boolean.js
@@ -0,0 +1,8 @@
+// boolean adalah salah satu tipe data yang menentukan nilai benar atau salah
+
+const isEnabled = true; // benar
+const isDisabled = false; // salah
+
+// contoh penggunaan boolean dalam kondisi
+console.log("nilai isEnabled: " + isEnabled);
+console.log("nilai isDisabled: " + isDisabled);
