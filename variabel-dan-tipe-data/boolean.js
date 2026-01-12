ef561b40029f0c776f8814356f17813f2fce6534
Alvin Aldino Rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-12 17:04:48 +0700

    Delete variabel-dan-tipe-data directory

--- variabel-dan-tipe-data/boolean.js
+++
@@ -1,8 +0,0 @@
-// boolean adalah salah satu tipe data yang menentukan nilai benar atau salah
-
-const isEnabled = true; // benar
-const isDisabled = false; // salah
-
-// contoh penggunaan boolean dalam kondisi
-console.log("nilai isEnabled: " + isEnabled);
-console.log("nilai isDisabled: " + isDisabled);
