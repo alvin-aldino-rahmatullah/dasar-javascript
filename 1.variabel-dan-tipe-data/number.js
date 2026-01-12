1e78fc7e91c1988fcc9bf0f088b24d915324ab97
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-12 17:10:49 +0700

    Arithmatic Operators

--- 1.variabel-dan-tipe-data/number.js
+++ 1.variabel-dan-tipe-data/number.js
@@ -16,16 +16,3 @@ console.log("jika dikurang: " + pengurangan);
 console.log("jika dikali: " + perkalian);
 console.log("jika dibagi: " + pembagian);
 console.log("jika sisa pembagian: " + sisaPembagian);
-
-console.log("-----------------------");
-// contoh kasus operasi number pada kehidupan sehari-hari
-const hargaSabun = 5000;
-const hargaShampoo = 15000;
-const diskon = 0.1; // 10% diskon
-const totalHarga = hargaSabun + hargaShampoo;
-const hargaSetelahDiskon = totalHarga - (totalHarga * diskon);
-
-console.log("harga sabun: " + hargaSabun);
-console.log("harga shampoo: " + hargaShampoo);
-console.log("Total harga sebelum diskon: " + totalHarga);
-console.log("Total harga setelah diskon 10%: " + hargaSetelahDiskon);
