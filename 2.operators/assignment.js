b1341ecbf58742764ce573d5112d9361018a7b09
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-13 18:50:27 +0700

    Operator Assignment.js

---
+++ 2.operators/assignment.js
@@ -0,0 +1,20 @@
+// assignment adalah proses pemberian nilai pada sebuah variabel menggunakan operator "=".
+// assignment ini sangat penting dalam pemrograman karena memungkinkan kita untuk menyimpan dan mengelola data dalam program kita.
+// seperti awalnya 5 bisa di ubah menjadi nilai lain seperti 10 atau 15 dengan menggunakan operator assignment ini.
+
+// contoh penggunaan operator assignment
+let x = 10; // menggunakan operator "=" untuk menetapkan nilai 10 ke variabel x
+let y = 5;  // menggunakan operator "=" untuk menetapkan nilai 5 ke variabel y
+
+// kita juga dapat menggunakan operator assignment untuk memperbarui nilai variabel
+x = x + 5; // menambahkan 5 ke nilai x dan memperbarui nilai x
+console.log("nilai x setelah penambahan: " + x); // output: 15
+
+// atau penulisan singkatnya bisa seperti ini
+y -= 3; // mengurangi 3 dari nilai y dan memperbarui nilai y
+console.log("nilai y setelah pengurangan: " + y); // output: 2
+
+// nah assignment ini tidak hanya terbatas pada operator "=" saja, 
+// ada juga operator assignment lainnya seperti "+=", "-=", "*=", "/=", dan "%=" yang digunakan untuk melakukan operasi matematika sekaligus memperbarui nilai variabel tersebut.
+// ya tinggal disesuaikan dengan kebutuhan operasi yang ingin dilakukan pada variabel tersebut.
+// singkatnya hanya perlu menggabungkan operator aritmatika dengan operator assignment "="
