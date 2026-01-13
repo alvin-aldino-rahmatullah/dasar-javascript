bbdedbda6dc0be26af44f80fd979cd80197cf783
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-13 18:10:59 +0700

    aritmatika operator

---
+++ 2.operators/aritmatika.js
@@ -0,0 +1,20 @@
+// aritmatika adalah operasi matematika dasar yang melibatkan penjumlahan, pengurangan, perkalian, pembagian, dan modulus
+// operator aritmatika digunakan untuk melakukan operasi tersebut pada nilai numerik
+
+const a = 10; 
+const b = 5;
+
+// operasi aritmatika dapat dilakukan menggunakan operator berikut:
+const penjumlahan = a+b; // (+) berfungsi untuk menjumlahkan nilai a dan b 
+const pengurangan = a-b; // (-) berfungsi untuk mengurangi nilai b dari a
+const perkalian = a*b; // (*) berfungsi untuk mengalikan nilai a dengan nilai b
+const pembagian = a/b; // (/) berfungsi untuk membagi nilai a dengan nilai b
+const modulus = a%b; // (%) berfungsi untuk menghasilkan sisa dari pembagian a dengan b
+
+// setiap + - * / % adalah operator aritmatika yang digunakan untuk melakukan operasi matematika pada nilai numerik
+
+console.log("hasil dari a + b adalah: " + penjumlahan);
+console.log("hasil dari a - b adalah: " + pengurangan);
+console.log("hasil dari a * b adalah: " + perkalian);
+console.log("hasil dari a / b adalah: " + pembagian);
+console.log("hasil dari a % b adalah: " + modulus);
