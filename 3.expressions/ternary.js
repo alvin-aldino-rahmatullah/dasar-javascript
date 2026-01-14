c0e8649d2bbf372f9ef558a9efa48efb26bcd300
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-14 19:08:59 +0700

    Update Expressions

--- 3.expressions/ternary.js
+++ 3.expressions/ternary.js
@@ -1,5 +1,18 @@
+// Ternary expression adalah bentuk singkat dari if else tetapi MASIH termasuk ekspresi karena menghasilkan nilai
+
+// Seperti :
+// kondisi ? nilai_jika_true : nilai_jika_false
+
+// Cara kerjanya 
+// 1. Program mengecek kondisi
+// 2. jika true → ambil nilai pertama
+// 3. jika false → ambil nilai kedua
+
 const nilai = 75;
 
+// cek apakah nilai >= 70
+// jika true → "LULUS"
+// jika false → "TIDAK LULUS"
 const statusKelulusan = nilai >= 70 ? "LULUS" : "TIDAK LULUS";
 
 console.log("status kelulusan:", statusKelulusan);
