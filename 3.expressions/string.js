4a948e9c1d6559d0645a133cd48d34ddd54e0afb
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-14 19:02:49 +0700

    Expressions String

---
+++ 3.expressions/string.js
@@ -0,0 +1,10 @@
+const firstName = "Bagus";
+const lastName = "Pratama";
+
+
+const fullName = firstName + " " + lastName;
+
+const greeting = "Halo, nama saya " + fullName;
+
+console.log(fullName);
+console.log(greeting);
