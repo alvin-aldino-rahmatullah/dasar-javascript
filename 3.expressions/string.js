5b9b6ce36580aea84638b806f39e99ca360b5bdc
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-14 19:05:20 +0700

    Update Expressions

--- 3.expressions/string.js
+++ 3.expressions/string.js
@@ -1,9 +1,13 @@
+// string expression adalah ekspresi yang menghasilkan string
+// biasanya menggunakan operator +
+
 const firstName = "Bagus";
 const lastName = "Pratama";
 
-
+// Program akan menggabungkan dua nama (String) menjadi satu
+// "Bagus" + " " + "Pratama"
 const fullName = firstName + " " + lastName;
-
+// Maka output yang dihasilkan bukan lagi nama terpisah tapi nama yang telah bersatu
 const greeting = "Halo, nama saya " + fullName;
 
 console.log(fullName);
