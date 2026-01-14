ba7fc01803a877f3e26f34aeced46d2b69e03ff1
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-14 18:47:48 +0700

    Exsperssions Aritmatika

---
+++ 3.expressions/aritmatika.js
@@ -0,0 +1,14 @@
+const x = 20;
+const y = 4;
+
+const hasilTambah = x + y;     
+const hasilKurang = x - y;
+const hasilKali = x * y;
+const hasilBagi = x / y;
+const hasilModulus = x % y;
+
+console.log("hasil x + y =", hasilTambah);
+console.log("hasil x - y =", hasilKurang);
+console.log("hasil x * y =", hasilKali);
+console.log("hasil x / y =", hasilBagi);
+console.log("hasil x % y =", hasilModulus);
