bba558d1fd8a5ce925ee4c7b850b8207a168554e
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-14 18:58:26 +0700

    Update Expressions

--- 3.expressions/logika.js
+++ 3.expressions/logika.js
@@ -1,9 +1,26 @@
+// logical expression digunakan untuk menggabungkan
+// satu atau lebih boolean expression
+
+// operator logika yang dipakai:
+// &&  (AND) maka true jika SEMUA true
+// ||  (OR)  maka true jika SALAH SATU true
+// !   (NOT) maka membalik nilai boolean
+
 const isLoggedIn = true;
 const isVerified = false;
+
+// JavaScript membaca:
+// isLoggedIn = true
+// isVerified = false
+// aturan && : semua harus true
+// hasilnya jadi false
 const canAccess = isLoggedIn && isVerified;
 
+// aturan || : salah satu true jadi true
 const canLogin = isLoggedIn || isVerified;
 
+// operator ! membalik nilai
+// false menjadi true
 const isNotVerified = !isVerified;
 
 console.log("hasil isLoggedIn && isVerified:", canAccess);
