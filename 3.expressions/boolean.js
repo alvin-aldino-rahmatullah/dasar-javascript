6e4941979c32ec2da26cb28cb164259aa35bf367
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-14 18:55:01 +0700

    Update Expressions

--- 3.expressions/boolean.js
+++ 3.expressions/boolean.js
@@ -1,6 +1,13 @@
+// boolean expression adalah ekspresi yang menghasilkan nilai true atau false
+// biasanya menggunakan operator perbandingan seperti: > < >= <= === !==
+
 const umur = 20;
 
+// apakah nilai umur (20) lebih besar atau sama dengan 18?
+// jika iya maka true
+// jika tidak maka false
 const isDewasa = umur >= 18;   
+// apakah nilai umur (20) lebih kecil dari 18?
 const isAnak = umur < 18;
 
 console.log("apakah umur >= 18?", isDewasa);
